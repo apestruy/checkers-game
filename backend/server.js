f63fd243260a8895a3d4de4f807377704493cb83
Pestruyeva <pestruya@mskcc.org>
2024-06-25 14:07:46 -0400

    fixed path module import issue

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import session from "express-session";
 import cors from "cors";
 
 import { fileURLToPath } from "url";
+import path from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
