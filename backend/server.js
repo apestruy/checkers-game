96e97998e58c4dc2fb35ddc643080453af652099
Pestruyeva <pestruya@mskcc.org>
2024-06-25 15:04:18 -0400

    adjusted server setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 import Player from "./models/Player.js";
 import Game from "./models/Game.js";
